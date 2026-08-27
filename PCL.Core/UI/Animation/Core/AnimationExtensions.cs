d49ef8973e7977beaff2d8c5e054604d4cfbe8d3
Hill23333 <a18035135032@163.com>
2026-08-27 20:38:15 +0800

    feat(animation): 添加支持多种动画属性的快速调用逻辑

--- PCL.Core/UI/Animation/Core/AnimationExtensions.cs
+++ PCL.Core/UI/Animation/Core/AnimationExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using PCL.Core.UI.Animation.Animatable;
 using PCL.Core.UI.Animation.Easings;
 
 namespace PCL.Core.UI.Animation.Core;
@@ -48,16 +50,16 @@ public static class AnimationExtensions
         double? width = null,
         double? height = null,
         double? opacity = null,
-        double? radius = null,
-        TranslateTransform? translate = null,
+        CornerRadius? radius = null,
+        NTranslateTransform? translate = null,
         double? translateX = null,
         double? translateY = null,
-        RotateTransform? rotate = null,
+        NRotateTransform? rotate = null,
         double? rotateAngle = null,
-        ScaleTransform? scale = null,
+        NScaleTransform? scale = null,
         double? scaleX = null,
         double? scaleY = null,
-        SkewTransform? skew = null,
+        NSkewTransform? skew = null,
         double? skewX = null,
         double? skewY = null,
         Thickness? margin = null,
@@ -73,7 +75,365 @@ public static class AnimationExtensions
         NColor? background = null,
         NColor? foreground = null)
     {
-        // TODO: 实现快速调用动画逻辑
+        duration = duration ?? TimeSpan.FromMilliseconds(100);
+        delay = delay ?? TimeSpan.Zero;
+        easing = easing ?? LinearEasing.Shared;
+
+        var aniGroup = new ParallelAnimationGroup();
+
+        // 这是全 CE 最石的代码（没有之一），你敢直视他 1 秒吗？
+
+        if (width is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = width.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.WidthProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (height is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = height.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.HeightProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (opacity is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = opacity.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, UIElement.OpacityProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (radius is not null)
+        {
+            var ani = new CornerRadiusFromToAnimation
+            {
+                To = radius.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Border.CornerRadiusProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (translate is not null)
+        {
+            var ani = new NTranslateTransformFromToAnimation
+            {
+                To = translate.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, UIElement.RenderTransformProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (translateX is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = translateX.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, TranslateTransform.XProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (translateY is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = translateY.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, TranslateTransform.YProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (rotate is not null)
+        {
+            var ani = new NRotateTransformFromToAnimation
+            {
+                To = rotate.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, UIElement.RenderTransformProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (rotateAngle is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = rotateAngle.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, RotateTransform.AngleProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (scale is not null)
+        {
+            var ani = new NScaleTransformFromToAnimation
+            {
+                To = scale.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, UIElement.RenderTransformProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (scaleX is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = scaleX.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, ScaleTransform.ScaleXProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (scaleY is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = scaleY.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, ScaleTransform.ScaleYProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (skew is not null)
+        {
+            var ani = new NSkewTransformFromToAnimation
+            {
+                To = skew.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, UIElement.RenderTransformProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (skewX is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = skewX.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, SkewTransform.AngleXProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (skewY is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = skewY.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, SkewTransform.AngleYProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (margin is not null)
+        {
+            var ani = new ThicknessFromToAnimation
+            {
+                To = margin.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.MarginProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (marginLeft is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = marginLeft.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.MarginProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (marginTop is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = marginTop.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.MarginProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (marginRight is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = marginRight.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.MarginProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (marginBottom is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = marginBottom.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, FrameworkElement.MarginProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (padding is not null)
+        {
+            var ani = new ThicknessFromToAnimation
+            {
+                To = padding.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.PaddingProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (paddingLeft is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = paddingLeft.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.PaddingProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (paddingTop is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = paddingTop.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.PaddingProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (paddingRight is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = paddingRight.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.PaddingProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (paddingBottom is not null)
+        {
+            var ani = new DoubleFromToAnimation
+            {
+                To = paddingBottom.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.PaddingProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (background is not null)
+        {
+            var ani = new NColorFromToAnimation
+            {
+                To = background.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.BackgroundProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        if (foreground is not null)
+        {
+            var ani = new NColorFromToAnimation
+            {
+                To = foreground.Value,
+                Duration = duration.Value,
+                Delay = delay.Value,
+                Easing = easing
+            };
+            SetTargetProperty(ani, Control.ForegroundProperty);
+            aniGroup.Children.Add(ani);
+        }
+
+        aniGroup.RunFireAndForget(EmptyAnimatable.Instance);
     }
-    
 }
